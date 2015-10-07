b301e4438e8e895a1e39bfa0c17849afd1d422fe
paulhendricks <paul.hendricks.2013@owu.edu>
2015-10-07 17:46:15 +0000

    some modifications

--- assets/_js/helpers/money.js
+++
@@ -1,7 +0,0 @@
-var accounting = require('accounting');
-
-function money(amount) {
-  return amount && accounting.formatMoney(amount);
-}
-
-module.exports = money;
--- assets/_js/pages/basket.js
+++
@@ -1,5 +0,0 @@
-var JSE = require('jekyll-store-engine');
-
-if(document.getElementById('basket-page')) {
-  JSE.Actions.checkoutStep({ step: 1 });
-}
